7d3df0f0099ee00baad0b0e8ba4db4931ba86c91
jnayeh <mouradslim44@gmail.com>
2021-12-31 13:44:59 +0100

    Added Search

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gstock/Screens/ComposantScreen.dart';
-import 'package:gstock/Screens/MembreScreen.dart';
 
 import 'Screens/LoginForm.dart';
 
@@ -19,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MembreScreen(),
+      home: LoginForm(),
     );
   }
 }
